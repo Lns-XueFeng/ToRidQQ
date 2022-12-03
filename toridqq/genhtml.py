4ea78398bc051745302b661896c3b5188a931d2c
handandkeep <1477792904@qq.com>
2022-12-03 22:28:12 +0800

    update

--- toridqq/genhtml.py
+++ toridqq/genhtml.py
@@ -4,20 +4,22 @@ import os
 class GenerateHtml:
     def __init__(self):
         self.dir_of_list = os.listdir("./images")
-        self.img_html = ""
+        self.img_html = None
+        self.style = "<style>html body{margin: 0 auto;text-align:center;color:pink;background:lightyellow;}" \
+                     "img {border:purple 4px solid;}</style>"
 
     def get_img_html(self):
         img_html = ""
-        for dir in self.dir_of_list:
-            if dir != "model":
-                new_img_label = f"<h2>{dir}</h2><img src='./images/{dir}/new_pic.png' alt=''>"
-                old_img_label = f"<img src='./images/{dir}/old_pic.png' alt=''>"
+        for dr in self.dir_of_list:
+            if dr != "model":
+                new_img_label = f"<h2>{dr}</h2><img src='./images/{dr}/new_pic.png' alt=''>"
+                old_img_label = f"<img src='./images/{dr}/old_pic.png' alt=''>"
                 img_html = img_html + new_img_label + old_img_label
         return img_html
 
     def get_html(self):
         return f"<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'>" \
-               f"<title>消息通知</title></head><body><h1>查看消息</h1><div>{self.img_html}</div></body></html>"
+               f"<title>消息通知</title>{self.style}</head><body><h1>查看消息</h1><div>{self.img_html}</div></body></html>"
 
     def gen_html_file(self):
         self.img_html = self.get_img_html()
